c2af243be8d614036a5d9f53ef844934e1b72d16
Razwan Rashidi <razwan.izmi@gmail.com>
2023-02-25 08:06:44 +0800

    Reformat all lua files

--- .config/nvim/lua/razwanizmi/plugins/lsp/mason.lua
+++ .config/nvim/lua/razwanizmi/plugins/lsp/mason.lua
@@ -22,12 +22,12 @@ mason.setup()
 mason_lspconfig.setup({
 	-- list of servers for mason to install
 	ensure_installed = {
-		"tsserver",
-		"html",
 		"cssls",
-		"tailwindcss",
-		"lua_ls",
 		"emmet_ls",
+		"html",
+		"lua_ls",
+		"tailwindcss",
+		"tsserver",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
@@ -36,9 +36,9 @@ mason_lspconfig.setup({
 mason_null_ls.setup({
 	-- list of formatters & linters for mason to install
 	ensure_installed = {
+		"eslint_d", -- ts/js linter
 		"prettier", -- ts/js formatter
 		"stylua", -- lua formatter
-		"eslint_d", -- ts/js linter
 	},
 	-- auto-install configured formatters & linters (with null-ls)
 	automatic_installation = true,
